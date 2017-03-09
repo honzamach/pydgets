5ca4ce19fc2d9b5f41441fb9163810f8ca502e79
Honza Mach <honza.mach.ml@gmail.com>
2017-03-09 10:45:11 +0100

    Improved overall package structure
    Following minor updates:
     - improved deployment makefile
     - gitignore file added for versioning
     - improved default readme and license files
     - improved Python package setup (added unit test support)
     - generated and deployed latest version to PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
-# Copyright (C) 2015-2016 Jan Mach <honza.mach.ml@gmail.com>
+# Copyright (C) 2015-2016 Jan Mach <email@jan-mach.cz>
 # Use of this source is governed by the MIT license, see LICENSE file.
 #-------------------------------------------------------------------------------
+
+# Resources:
+#   https://packaging.python.org/en/latest/
+#   https://python-packaging.readthedocs.io/en/latest/index.html
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
@@ -16,10 +20,10 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
-    
+
 setup(
     name = 'pydgets',
-    version = '0.3',
+    version = '0.6',
     description = 'Console widget library for Python 3',
     long_description = long_description,
     classifiers = [
@@ -34,6 +38,8 @@ setup(
     author_email = 'email@jan-mach.cz',
     license = 'MIT',
     packages = ['pydgets'],
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
     include_package_data = True,
     zip_safe = False
 )
